aa7cab02fb642a15c3f8ff95ddfa4b2aba980860
Muhammad Mehboob Ahmed <mehboob.ahmed@netsoltech.com>
2019-08-28 10:49:00 +0500

    Commit

--- Integration Workspace/Source Code/test.py
+++ Integration Workspace/Source Code/test.py
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 print ("My Name Is Mehboob")
 print ("I Am Muslim")
 print ("I Live In Lahore")
 print ("I Love PAKISTAN")
 print ("I AM 29 Years Old")
 print ("I Work In A Software Company")
+=======
+print ("My Name Is Mehboob")
+>>>>>>> parent of acc4f5b... Information Added
